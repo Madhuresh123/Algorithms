aec9929c876bfe8d162a3a6ea75e4b700fed2c12
Madhuresh <55894893+Madhuresh123@users.noreply.github.com>
2023-09-08 13:00:38 +0530

    Time: 87 ms (47.41%) | Memory: 52.7 MB (33.05%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,23 @@
+function rotate(nums, k) {
+    k = k % nums.length;
+
+    // Reverse the entire array
+    reverse(nums, 0, nums.length - 1);
+
+    // Reverse the first k elements
+    reverse(nums, 0, k - 1);
+
+    // Reverse the remaining elements
+    reverse(nums, k, nums.length - 1);
+}
+
+// Helper function to reverse a portion of the array in-place
+function reverse(nums, start, end) {
+    while (start < end) {
+        const temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
