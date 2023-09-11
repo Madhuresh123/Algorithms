22750bcaa8e14277c31315ab75ed63484fe00897
Madhuresh <55894893+Madhuresh123@users.noreply.github.com>
2023-09-11 09:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (67.84%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,39 +1,44 @@
 public class Solution {
     public void nextPermutation(int[] nums) {
+
+        int start = -1;
         int n = nums.length;
-        int i = n - 2;
 
-        // Find the first decreasing element from the right
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        if (i >= 0) {
-            // Find the smallest element to the right of nums[i] that is larger than nums[i]
-            int j = n - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
+        for(int i=n-2; i>=0 ; i--){
+            if(nums[i] < nums[i+1]){
+                start = i;
+                break;
             }
-
-            // Swap nums[i] and nums[j]
-            swap(nums, i, j);
         }
 
-        // Reverse the subarray to the right of i
-        reverse(nums, i + 1, n - 1);
+        if(start == -1){
+            reverse(nums, 0, n-1);
+        }else{
+
+            for(int i=n-1; i>=0; i--){
+                if(nums[i] > nums[start]){
+                    swap(nums,i,start);
+                    break;
+                }
+            }
+            reverse(nums, start+1, n-1);
+        }
+      
     }
 
-    private void swap(int[] nums, int a, int b) {
+    public void swap(int[] nums, int a, int b){
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
     }
 
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
-        }
+    public void reverse(int[] nums, int a, int b){
+
+            while(a <= b){
+                swap(nums, a,b);
+                a++;
+                b--;
+            }
     }
+
 }
